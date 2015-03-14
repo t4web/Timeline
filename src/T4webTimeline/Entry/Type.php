2e3bc9182a16c0c2f547e2fc002b622dc2d1c96e
maxgu <maxgu@t4web.com.ua>
2015-03-14 14:02:42 +0200

    add Entry\Type

---
+++ src/T4webTimeline/Entry/Type.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace T4webEmployees\Employee;
+
+use T4webBase\Domain\Enum;
+
+class Type extends Enum {
+    
+    const USER_CREATED = 1;
+    const USER_UPDATED = 2;
+    const USER_DELETED = 3;
+
+    /**
+     * @var array
+     */
+    protected static $constants = array(
+        self::USER_CREATED => 'User created',
+        self::USER_UPDATED => 'User updated',
+        self::USER_DELETED => 'User deleted',
+    );
+
+}
