81d106c3daa78367c2cf3968b7ba0f559b16cedf
maxgu <maxgu@t4web.com.ua>
2015-03-14 12:17:47 +0200

    remove listener

--- Module.php
+++ Module.php
@@ -6,25 +6,15 @@ use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\ControllerProviderInterface;
 use Zend\ModuleManager\Feature\ConsoleUsageProviderInterface;
 use Zend\ModuleManager\Feature\ServiceProviderInterface;
-use Zend\ModuleManager\Feature\BootstrapListenerInterface;
 use Zend\Mvc\Controller\ControllerManager;
 use Zend\Console\Adapter\AdapterInterface as ConsoleAdapterInterface;
 use Zend\ServiceManager\ServiceManager;
-use Zend\EventManager\EventInterface;
 use T4webTimeline\Controller\Console\InitController;
 
 class Module implements AutoloaderProviderInterface, ConfigProviderInterface,
                         ControllerProviderInterface, ConsoleUsageProviderInterface,
-                        ServiceProviderInterface, BootstrapListenerInterface
+                        ServiceProviderInterface
 {
-    public function onBootstrap(EventInterface $e)
-    {
-        $em  = $e->getApplication()->getEventManager();
-        $sm  = $e->getApplication()->getServiceManager();
-
-        $employeesListener = $sm->get('T4webTimeline\Listener\Employees');
-        $employeesListener->attach($em);
-    }
 
     public function getConfig($env = null)
     {
@@ -53,10 +43,6 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface,
     {
         return array(
             'factories' => array(
-                'T4webTimeline\Listener\Employees' => function(ServiceManager $sm) {
-                    return new Listener\Employees(
-                    );
-                },
             ),
             'invokables' => array(
             ),
